9fdf88ce7f905b01489d1e581ae56a605354056e
Casey Chow <casey.chow@gmail.com>
2016-12-27 22:27:26 -0500

    fix static, allow production

--- easypce/settings.py
+++ easypce/settings.py
@@ -7,6 +7,7 @@ if len(sys.argv) > 1:
 else:
     DEBUG = False
 
+DEBUG = True #TODO: Remove.
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -72,6 +73,10 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+# Static File Serving
+# https://devcenter.heroku.com/articles/django-assets
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '../templates/')
@@ -81,18 +86,17 @@ MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '../templates/')
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), '/static/')
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (	
+STATICFILES_DIRS = (    
+    os.path.join(PROJECT_ROOT, 'static'),
     os.path.join(os.path.dirname(__file__), '../templates/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
