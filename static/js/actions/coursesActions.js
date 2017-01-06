0192aeefd676f222b51893912621491a3b9effdb
Evan Wildenhain <mew2@princeton.edu>
2017-01-06 01:07:14 -0800

    fixing typo in coursesActions.js

--- static/js/actions/coursesActions.js
+++ static/js/actions/coursesActions.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export function fetchCourses() {
   return function(dispatch) {
-    axios.get("/api/terms")
+    axios.get("/api/courses")
       .then((response) => {
-        dispatch({type: "FETCH_COURSES_FULFILLED", payload: response})
+        dispatch({type: "FETCH_COURSES_FULFILLED", payload: response.data})
       })
       .catch((err) => {
         dispatch({type: "FETCH_COURSES_REJECTED", payload: err})
