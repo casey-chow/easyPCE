b81d2b92fb5c68b38bd5a418c589ce916b389eee
Casey Chow <casey.chow@gmail.com>
2017-02-02 09:14:30 -0500

    streamline production webpack config

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,11 +1,7 @@
-const path = require('path');
 const webpack = require('webpack');
 const BundleTracker = require('webpack-bundle-tracker');
 
 const config = require('./common.config.js');
-
-// export to dist directory during production
-config.output.path = path.resolve(__dirname, '../dist');
 
 config.devtool = 'source-map';
 
@@ -26,12 +22,5 @@ config.plugins = config.plugins.concat([
         },
     }),
 ]);
-
-// Add a loader for JSX files
-config.module.loaders.push({
-    test: /\.jsx?$/,
-    exclude: /node_modules/,
-    loader: 'babel-loader',
-});
 
 module.exports = config;
